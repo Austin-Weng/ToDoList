6bc4c44aef1f331546e771101f33c13cc91ea1ab
Austin-Weng <73495049+Austin-Weng@users.noreply.github.com>
2024-02-29 23:28:55 -0800

    Fleshed out idea, hope to get the AddTaskBar to actually add a task

--- ToDoList/App.js
+++ ToDoList/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, FlatList} from 'react-native';
+import { StyleSheet, View, Text, FlatList, StatusBar,} from 'react-native';
 
 const tasks = [
   { key: 'Work on expo project' },
@@ -8,20 +8,29 @@ const tasks = [
   { key: 'Collision Rewrite'},
   { key: 'Assignment #8 - Computing Integrals' },
 ];
+const doneTasks = [
+  
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
+      <tr>
+        <th style={styles.header}>To-Do</th>
+      </tr>
       <FlatList
         data={tasks}
-        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        renderItem={({item}) => <Text style={styles.taskItem}>{item.key}</Text>}
       />
-      <addTaskBar style={styles.addTaskBar}/>
+      <tr>
+        <th style={styles.completed}> Completed </th>
+      </tr>
+      <AddTaskBar style={styles.addTaskBar}/>
     </View>
   );
 
-  function addTaskBar() {
+  function AddTaskBar() {
     return (
       <form>
         <input type="text" placeholder="Click here to add a task" />
@@ -35,16 +44,29 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  completed: {
+    fontSize: 10,
+    alignItems: 'left',
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: 30,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#00000080', // Semi-transparent background
+    backgroundColor: 'lavender', // Semi-transparent background
+    textAlign: 'center',
+    alignItems: 'center',
   },
   taskItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ffffff50', // Semi-transparent border
+    padding: 15,
+    margin: 5,
+    borderWidth: 3,
+    borderRadius: 5,
+    borderColor: 'black', // Semi-transparent border
   },
   taskText: {
     color: 'white',
@@ -52,7 +74,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   addTaskBar: {
-    padding: 10,
+    margin: 10,
+    position: 'absolute',
+    left:0,
+    bottom:0,
+    right:0,
     borderWidth: 1,
     borderRadius: 10,
     borderColor: 'blue',
